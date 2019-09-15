efa50eae49c0ba09234033078d27c61a847333fb
k-skye <36499136+k-skye@users.noreply.github.com>
2019-09-16 00:26:12 +0800

    完成tabbar底部栏

--- vue-ai-school-takeaway/src/router.js
+++ vue-ai-school-takeaway/src/router.js
@@ -8,6 +8,32 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
+      path: '/',
+      // name: 'index',
+      component: () => import('./views/index.vue'),
+      children: [
+        {
+          path: '',
+          redirect: '/home'
+        },
+        {
+          path: '/home',
+          name: 'home',
+          component: () => import('./views/Home.vue')
+        },
+        {
+          path: '/order',
+          name: 'order',
+          component: () => import('./views/Order.vue')
+        },
+        {
+          path: '/me',
+          name: 'me',
+          component: () => import('./views/Me.vue')
+        }
+      ]
+    },
+    {
       path: '/login',
       name: 'login',
       component: () => import('./views/Login.vue')
